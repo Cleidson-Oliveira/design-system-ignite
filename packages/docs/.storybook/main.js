e3bb000d3cbc024ab2e399fd25751e3770847fa7
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-04-21 18:35:37 -0300

    fix: restore files

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,17 +1,30 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+
+import { mergeConfig } from 'vite';
+
 const config = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-docs",
+        // "storybook-addon-preview",
+        "@storybook/addon-a11y"
+    ],
+    core: {
+        builder: '@storybook/builder-vite',
+    },
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+    async viteFinal (config, { configType }) {
+        if (configType === 'PRODUCTION') {
+            config.base = '/design-system-ignite/'
+        }
+
+        return mergeConfig(config);
+    }
 };
 export default config;
