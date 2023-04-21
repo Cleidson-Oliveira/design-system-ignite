54bb171c4a990eede483fbd2a157ad4e781a5081
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-04-21 18:11:27 -0300

    chore: add storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,30 +1,17 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-
-import { mergeConfig } from 'vite';
-
 const config = {
-    stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        "@storybook/addon-docs",
-        "storybook-addon-preview",
-        "@storybook/addon-a11y"
-    ],
-    core: {
-        builder: '@storybook/builder-vite',
-    },
-    framework: {
-        name: "@storybook/react-vite",
-        options: {},
-    },
-    async viteFinal (config, { configType }) {
-        if (configType === 'PRODUCTION') {
-            config.base = '/design-system-ignite/'
-        }
-
-        return mergeConfig(config);
-    }
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
